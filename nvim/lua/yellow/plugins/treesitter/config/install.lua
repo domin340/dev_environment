309600fdb6d6bb8a103bed6ac8e9e486c52e90db
domin340 <poweski2009@gmail.com>
2025-04-17 18:18:16 +0200

    nothing

--- nvim/lua/yellow/plugins/treesitter/config/install.lua
+++ nvim/lua/yellow/plugins/treesitter/config/install.lua
@@ -12,6 +12,6 @@ return {
 	"vimdoc",
 	"c",
 	"cpp",
-	"haxe",
 	"go",
+	"haxe",
 }
--- nvim/lua/yellow/plugins/treesitter/parsers/setup.lua
+++ nvim/lua/yellow/plugins/treesitter/parsers/setup.lua
@@ -5,7 +5,8 @@ local parsers = {
 return function(configs, languagues)
 	for _, name in ipairs(languagues) do
 		if parsers[name] then
-			configs[name] = parsers[name]()
+			local conf = parsers[name]()
+			configs[name] = conf
 		end
 	end
 end
